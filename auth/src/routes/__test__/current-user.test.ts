33823f4b0de9deceb6ac744d716ffa46859502ad
Rahul Nallappa <rahul.nallappa@gmail.com>
2020-12-08 11:39:48 -0600

    fixed test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -6,7 +6,7 @@ it('responds with null if not authenticated', async () => {
   const response = await request(app)
     .get('/api/users/currentuser')
     .send()
-    .expect(404);
+    .expect(200);
 
   expect(response.body.currentUser).toEqual(null);
 });
